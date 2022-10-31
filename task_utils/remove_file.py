6375717225bca10b857155ad66a841db07242a3a
Jainal Gosaliya <jainal09gosalia@gmail.com>
2022-10-31 02:54:22 -0400

    Update remove_file.py

--- task_utils/remove_file.py
+++ task_utils/remove_file.py
@@ -9,8 +9,6 @@ def remove_api(file, last_file):
         'value': file,
         'client_id': DASHBOARD_API_CLIENT_ID
     }
-    try:
-        requests.request("POST", DASHBOARD_API_URL_REMOVE_FILE, data=payload)
 
-    except Exception as e:
-        init.send_log_msg(msg=str(e) + " EXCEPTION IN REMOVE FILE API CALL__", error=True)
+    requests.request("POST", DASHBOARD_API_URL_REMOVE_FILE, data=payload)
+    init.send_log_msg(msg=str(e) + " EXCEPTION IN REMOVE FILE API CALL__", error=True)
